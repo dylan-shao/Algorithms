e342d242dbd644b052d401eb1f0e67c6fb78a151
Dylan-Shao <yangshao2014@gmail.com>
2018-09-30 23:20:35 -0700

    update

--- DP_DFS/516. Longest Palindromic Subsequence/Solution.py
+++ DP_DFS/516. Longest Palindromic Subsequence/Solution.py
@@ -48,3 +48,33 @@ class Solution:
 
 
         return res
+
+
+#-------------------------- DFS --------------------------------------
+# See another solution in DP
+class Solution2:
+    def longestPalindromeSubseq(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        i = 0
+        j = len(s) - 1
+        return self.__dfs(s, i, j, {})
+
+    def __dfs(self, s, start, end, dic):
+        if start > end:
+            return 0
+        if start == end:
+            return 1
+
+        if (start,end) in dic:
+            return dic[(start,end)]
+
+        if s[start] == s[end]:
+            res = self.__dfs(s, start + 1, end - 1,dic) + 2
+            dic[(start,end)] = res
+        else:
+            res = max(self.__dfs(s, start + 1, end,dic),self.__dfs(s, start, end - 1,dic))
+            dic[(start,end)] = res
+        return res
