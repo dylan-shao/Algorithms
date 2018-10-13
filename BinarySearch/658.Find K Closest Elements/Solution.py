44197db46e79679f7fd881d571dbcf66e939502a
Dylan-Shao <yangshao2014@gmail.com>
2018-10-13 03:49:38 -0400

    658 py

---
+++ BinarySearch/658.Find K Closest Elements/Solution.py
@@ -0,0 +1,61 @@
+class Solution:
+    def findClosestElements(self, arr, k, x):
+        """
+        :type arr: List[int]
+        :type k: int
+        :type x: int
+        :rtype: List[int]
+        """
+        if len(arr) == 0 or k == 0:
+            return []
+
+        left = 0
+        right = len(arr) - 1
+        start = -1
+        res = []
+        while left < right - 1:
+            mid = left + (right - left) // 2
+
+            if arr[mid] == x:
+                start = mid
+                break
+            elif arr[mid] < x:
+                left = mid
+            else:
+                right = mid
+
+        # not found, or in left or right
+        if start == -1:
+            #edge case
+            if arr[left] > x:
+                return arr[:k]
+            if arr[right] < x:
+                return arr[-k:]
+            if x - arr[left] > arr[right] - x:
+                start = right
+            else:
+                start = left
+        left = start
+        right = start
+        res.append(arr[start])
+
+        while k > 1:
+            if left >= 0:
+                left -= 1
+            if right <= len(arr) - 1:
+                right += 1
+
+            if left < 0:
+                res.append(arr[right])
+            elif right > len(arr) - 1:
+                res = [arr[left]] + res
+
+            elif x - arr[left] <= arr[right] - x:
+                res = [arr[left]] + res
+                right -= 1
+            else:
+                res.append(arr[right])
+                left += 1
+            k -= 1
+        return res
+            
