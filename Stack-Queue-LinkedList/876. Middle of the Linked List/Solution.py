8226b6e83f383092a299af65fcf061e2b8d66b54
Dylan-Shao <yangshao2014@gmail.com>
2018-12-05 01:17:37 +0800

    876 py

---
+++ Stack-Queue-LinkedList/876. Middle of the Linked List/Solution.py
@@ -0,0 +1,22 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def middleNode(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+
+        if not head or not head.next:
+            return head
+
+        slow = node
+        fast = node
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
