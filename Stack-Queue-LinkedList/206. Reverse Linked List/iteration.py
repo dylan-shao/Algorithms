75a227ec1547e47175c40b8c336f71e28148bff9
Dylan-Shao <yangshao2014@gmail.com>
2018-12-05 00:17:29 +0800

    206 py

---
+++ Stack-Queue-LinkedList/206. Reverse Linked List/iteration.py
@@ -0,0 +1,47 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def reverseList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        if head == None or head.next == None:
+            return head
+
+        prev = None
+        cur = head
+
+        while cur.next:
+            next = cur.next
+            cur.next = prev
+            prev = cur
+            cur = next
+
+        cur.next = prev
+
+        return cur
+
+class Solution(object):
+    def reverseList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        if head == None or head.next == None:
+            return head
+
+        prev = None
+        cur = head
+
+        while cur:
+            next = cur.next
+            cur.next = prev
+            prev = cur
+            cur = next
+
+        return prev
